b49a9b489b64183108bae3f08f90d3e7aa880e38
xinchi.huo <850996480@qq.com>
2026-04-20 19:07:49 +0800

    init

--- Django_catalog/settings.py
+++ Django_catalog/settings.py
@@ -116,9 +116,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
 
 # 2. 远程通信配置
 # 主系统 API 的基础地址 (结尾需带斜杠)
